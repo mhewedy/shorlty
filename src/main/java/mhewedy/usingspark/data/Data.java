ae7c4584c4bd1c0ca4df2e46e92a91928fbe5af6
mhewedy <mohammed_a_hewedy@hotmail.com>
2014-05-02 19:29:54 +0300

    use stream instead of imprative for-loop

--- src/main/java/mhewedy/usingspark/data/Data.java
+++ src/main/java/mhewedy/usingspark/data/Data.java
@@ -19,15 +19,11 @@ public interface Data {
 
 	static String getOriginalURL(Data[] dataArray, String shortUrl) {
 		if (dataArray != null) {
-
 			Arrays.sort(dataArray, Comparator.comparing(d -> d.getPriority()));
-			for (Data data : dataArray) {
-				String url = data.getOriginalURL(shortUrl);
-				if (url != null && !url.isEmpty()) {
-					System.out.println("Getting original Url from: " + data);
-					return url;
-				}
-			}
+			return Arrays.stream(dataArray)
+				.map(d -> d.getOriginalURL(shortUrl))
+				.filter(u -> u != null && !u.isEmpty())
+				.findFirst().orElse("");
 		}
 		return "";
 	}
