32ad9de639183103a12fc380ced6625c2cc3eb5a
mhewedy <mohammed_a_hewedy@hotmail.com>
2014-05-05 01:54:02 +0300

    change the bootstarp to adapt parse.com cannot return more than 1k rows

--- src/main/java/mhewedy/usingspark/data/DataBootstraper.java
+++ src/main/java/mhewedy/usingspark/data/DataBootstraper.java
@@ -1,10 +1,11 @@
 package mhewedy.usingspark.data;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.parse4j.ParseException;
 import org.parse4j.ParseObject;
 import org.parse4j.ParseQuery;
 
@@ -13,24 +14,86 @@ public class DataBootstraper {
 	 * Should call on server start only!
 	 */
 	public static void bootstrap() {
-		ParseQuery<ParseObject> query = ParseQuery.getQuery(Columns.URL_MAPPING_CLASS).orderByDescending(
-				Columns.CREATED_AT);
-		try {
-			List<ParseObject> list = query.find();
+		System.out.println("start bootstraping....");
 
-			if (list != null && !list.isEmpty()) {
-				Base64Ops.resotreCurrentVal(list.get(0).getString(Columns.SHORT_URL_COL));
+		List<ParseObject> list = getAllData();
+
+		if (list != null && !list.isEmpty()) {
+			if (assertNoDuplicateInShortUrl(list)) {
+				String maxShortUrlValue = list.get(0).getString(Columns.SHORT_URL_COL);
+				Base64Ops.resotreCurrentVal(maxShortUrlValue);
+
+				System.out.println("max shourt_url value is " + maxShortUrlValue);
 
 				copyData(list);
 
 				System.out.println("done data bootstraping..");
 			} else {
-				System.out.println("data bootstrap => no data to operate on");
+				throw new RuntimeException("problem loading data ... exiting..");
 			}
-		} catch (ParseException e) {
-			System.err.println("cannot restore Parse.com records!");
-			throw new RuntimeException(e);
+
+		} else {
+			System.out.println("data bootstrap => no data to operate on");
 		}
+	}
+
+	private static boolean assertNoDuplicateInShortUrl(List<ParseObject> list) {
+		System.out.println("start validating duplicate short_url values");
+
+		int actualDataCount = 0;
+		int distinctDataCount = 0;
+		try {
+			List<String> listOfUniqueShortUrl = list.stream().map(o -> o.getString(Columns.SHORT_URL_COL)).distinct()
+					.collect(Collectors.toList());
+
+			actualDataCount = ParseQuery.getQuery(Columns.URL_MAPPING_CLASS).count();
+			distinctDataCount = listOfUniqueShortUrl.size();
+
+			if (actualDataCount == distinctDataCount) {
+				System.out.println("short_url data validated successfully");
+				return true;
+			}
+		} catch (Exception ex) {
+			System.err.println(ex.getMessage());
+		}
+
+		System.out.printf(
+				"duplicate shortUrls .. Actual object count is %d, distinct objects based on shortUrl count is %d\n",
+				actualDataCount, distinctDataCount);
+		System.out.flush();
+		return false;
+
+	}
+
+	private static List<ParseObject> getAllData() {
+		System.out.println("loading all data from db...");
+
+		ParseQuery<ParseObject> query = ParseQuery.getQuery(Columns.URL_MAPPING_CLASS).orderByDescending(
+				Columns.CREATED_AT);
+
+		List<ParseObject> list = new ArrayList<>();
+		try {
+			final int skip = 1000;
+
+			List<ParseObject> subList = null;
+			query.limit(skip);
+
+			int count = 1;
+			while ((subList = query.find()) != null) {
+				list.addAll(subList);
+				query.skip(skip * count++);
+			}
+		} catch (Exception ex) {
+			System.err.println("cannot restore Parse.com records!");
+			throw new RuntimeException(ex);
+		}
+		System.out.printf("done loading %d items\n", list.size());
+
+		System.out.println("Sorting descending by \"createAt\" ....");
+		list.stream().sorted(Comparator.comparing((ParseObject o) -> o.getDate(Columns.CREATED_AT)).reversed());
+		System.out.println("done sorting");
+
+		return list;
 	}
 
 	private static void copyData(List<ParseObject> list) {
